ea3ac2a6f93fb4af3270fc34e0286c674acf3ad1
bigppfrontdev <rauh.benu@gmail.com>
2021-06-19 14:35:18 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var searchword = "";
+
+   function category(){
+   	    document.getElementById("night").scrollIntoView();
+   }
+   function category1(){
+   	    document.getElementById("blood").scrollIntoView();
+   }
+   function category2(){
+   	    document.getElementById("drae").scrollIntoView();
+   }
+   function category3(){
+   	    document.getElementById("taur").scrollIntoView();
+   }
+
+   function category6(){
+   	    document.getElementById("dwar").scrollIntoView();
+   }
+    function category7(){
+   	    document.getElementById("hum").scrollIntoView();
+   }
